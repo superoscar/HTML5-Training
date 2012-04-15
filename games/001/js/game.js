2326ac5fb9dfcd7e31463d325a082c76366101a4
Oscar Wang <oscarwx@gmail.com>
2012-04-16 00:31:11 +0800

    the training exercises for HTML5 2012

---
+++ games/001/js/game.js
@@ -0,0 +1,232 @@
+﻿$(document).ready(function(){
+	var canvas = $("#gameCanvas");
+	var context= canvas.get(0).getContext("2d");
+	
+	var canvasWidth=canvas.width();
+	var canvasHeight=canvas.height();
+	
+	var playGame;
+	var stars;
+	var numStars;
+	var player;
+	var score;
+	var scoreTimeout;
+	var arrowUp = 38;
+	var arrowRight = 39;
+	var arrowDown = 40;
+	
+	var Player = function(x,y){
+		this.x = x;
+		this.y = y;
+		this.width=24;
+		this.height = 24;
+		this.halfWidth = this.width/2;
+		this.halfHeight = this.height/2;
+		this.vX = 0;
+		this.vY = 0;
+		this.moveRight = false;
+		this.moveUp = false;
+		this.moveDown = false;
+		this.flameLength = 20;
+	}
+	var Star = function(x,y,radius,vX){
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.vX = vX;
+	}
+	
+	var ui=$("#gameUI");
+	var uiInfo=$("#gameInfo");
+	var uiStats=$("#gameStats");
+	var uiComplete=$("#gameComplete");
+	var uiPlay=$("#gamePlay");
+	var uiReset=$(".gameReset");
+	var uiScore=$(".gameScore");
+	
+	function startGame(){
+		uiScore.html("0");
+		uiStats.show();
+		
+		playGame=false;
+		
+		stars = new Array();
+		numStars = 10;
+		score = 0;
+		player = new Player(150,canvasHeight/2);
+		
+		for(var i=0;i<numStars;i++){
+			var radius = 5 + (Math.random()*10);
+			var x=canvasWidth+radius+Math.floor(Math.random()*canvasWidth);
+			var y=Math.floor(Math.random()*canvasHeight);
+			var vX = -5-(Math.random()*5);
+			
+			stars.push(new Star(x,y,radius,vX));
+		};
+		
+		$(window).keydown(function(e){
+			var keyCode = e.keyCode;
+			if(!playGame){
+				playGame = true;
+				animate();
+				timer();
+			};
+			
+			if(keyCode == arrowRight){
+				player.moveRight=true;
+			}else if(keyCode==arrowUp){
+				player.moveUp=true;
+			}else if(keyCode == arrowDown){
+				player.moveDown=true;
+			};
+		});
+		$(window).keyup(function(e){
+			var keyCode = e.keyCode;
+			if(keyCode == arrowRight){
+				player.moveRight=false;
+			}else if(keyCode==arrowUp){
+				player.moveUp=false;
+			}else if(keyCode == arrowDown){
+				player.moveDown=false;
+			};
+		});
+		
+		animate();
+	};
+	
+	function init(){
+		uiStats.hide();
+		uiComplete.hide();
+		uiPlay.click(function(e){
+			e.preventDefault();
+			uiInfo.hide();
+			startGame();
+			});
+		uiReset.click(function(e){
+			e.preventDefault();
+			uiComplete.hide();
+			$(window).unbind("keyup");
+			$(window).unbind("keydown");
+			clearTimeout(scoreTimeout);
+			startGame();
+			});
+	};
+	
+	function timer(){
+		if(playGame){
+			scoreTimeout = setTimeout(function(){
+				uiScore.html(++score);
+				timer();
+				},1000);
+		};
+	};
+	
+	function animate(){
+		context.clearRect(0,0,canvasWidth,canvasHeight);
+		
+		
+		var starsLength = stars.length;
+		for(var i=0;i<starsLength;i++){
+			var tmpStar = stars[i];
+			tmpStar.x += tmpStar.vX;
+			if(tmpStar.x+tmpStar.radius<0){
+				tmpStar.radius=5+(Math.random()*10);
+				tmpStar.x = canvasWidth+tmpStar.radius;
+				tmpStar.y = Math.floor(Math.random()*canvasHeight);
+				tmpStar.vX = -5-(Math.random()*5);
+			};
+			
+			var dX = player.x - tmpStar.x;
+			var dY = player.y - tmpStar.y;
+			var distance = Math.sqrt((dX*dX)+(dY*dY));
+			
+			if(distance < player.halfWidth + tmpStar.radius){
+				playGame = false;
+				clearTimeout(scoreTimeout);
+				uiStats.hide();
+				uiComplete.show();
+				
+				$(window).unbind("keyup");
+				$(window).unbind("keydown");
+			};
+			
+			context.fillStyle="rgb(255,255,255)";
+			context.beginPath();
+			context.arc(tmpStar.x,tmpStar.y,tmpStar.radius,0,Math.PI*2,true);
+			context.closePath();
+			context.fill();
+		};
+		
+		
+		player.vX =0;
+		player.vY =0;
+		if(player.moveRight){
+			player.vX=3;
+		}else{
+			player.vX=-2;
+		};
+		if(player.moveUp){
+			player.vY=-3;
+		};
+		if(player.moveDown){
+			player.vY=3;
+		};
+		
+		
+		player.x+=player.vX;
+		player.y+=player.vY;
+		
+		if(player.x-player.halfWidth<20){
+			player.x=20+player.halfWidth;
+		}else if(player.x+player.halfWidth>canvasWidth-20){
+			player.x = canvasWidth-20-player.halfWidth;
+		};
+		if(player.y-player.halfHeight<20){
+			player.y=20+player.halfHeight;
+		}else if(player.y+player.halfHeight>canvasHeight-20){
+			player.y = canvasHeight-20-player.halfHeight;
+		};
+		
+		
+		if(player.moveRight){
+			context.save();
+			context.translate(player.x-player.halfWidth,player.y);
+			if(player.flameLength == 20){
+				player.flameLength = 15;
+			}else{
+				player.flameLength = 20;
+		};
+		context.fillStyle="orange";
+		context.beginPath();
+		context.moveTo(0,-5);
+		context.lineTo(-player.flameLength,0);
+		context.lineTo(0,5);
+		context.closePath();
+		context.fill();
+		
+		context.restore();
+		};
+		
+		context.fillStyle="rgb(200,120,0)";
+		context.beginPath();
+		context.moveTo(player.x+player.halfWidth,player.y);
+		context.lineTo(player.x-player.halfWidth,player.y-player.halfHeight);
+		context.lineTo(player.x-player.halfWidth,player.y+player.halfHeight);
+		context.closePath();
+		context.fill();
+		
+		while(stars.length<numStars){
+			var radius = 5 +(Math.random()*10);
+			var x = Math.floor(Math.random()*canvasWidth)+canvasWidth+radius;
+			var y = Math.floor(Math.random()*canvasHeight);
+			var vX = -5-(Math.random()*5);
+			
+			stars.push(new Star(x,y,radius,vX));
+		};
+		
+		if(playGame){
+			setTimeout(animate,33);
+		}
+	}
+	init();
+});
---
+++ games/002/js/game.js
@@ -0,0 +1,348 @@
+﻿$(document).ready(function(){
+	var canvas = $("#gameCanvas");
+	var context = canvas.get(0).getContext("2d");
+	
+	var canvasWidth = canvas.width();
+	var canvasHeight = canvas.height();
+	
+	var playGame;
+	var platformX;
+	var platformY;
+	var platformOuterRadius;
+	var platformInnerRadius;
+	
+	var stars;
+	
+	var player;
+	var playerOriginalX;
+	var playerOriginalY;
+	
+	var playerSelected;
+	var playerMaxAbsV;
+	var playerVd;
+	var powerX;
+	var powerY;
+	
+	var score;
+	
+	var ui = $("#gameUI");
+	var uiInfo = $("#gameInfo");
+	var uiStats = $("#gameStats");
+	var uiComplete = $("#gameComplete");
+	var uiPlay = $("#gamePlay");
+	var uiReset = $(".gameReset");
+	var uiRemaining = $("#gameRemaining");
+	var uiScore = $(".gameScore");
+	
+	//对象
+	var Star = function(x,y,radius,m,friction){
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.m = m;
+		this.friction = friction;
+		this.vX = 0;
+		this.vY = 0;
+		this.player = false;
+	};
+	
+	function resetPlayer(){
+		player.x = playerOriginalX;
+		player.y = playerOriginalY;
+		player.vX = 0;
+		player.vY = 0;
+	};
+	
+	function startGame(){
+		playGame = false;
+		platformX=canvasWidth/2;
+		platformY=150;
+		platformOuterRadius=100;
+		platformInnerRadius=75;
+		stars = new Array();
+		
+		playerSelected = false;
+		playerMaxAbsV = 30;
+		playerVd = 0.3;
+		powerX = -1;
+		powerY = -1;
+		
+		score = 0;
+		
+		var pRadius = 15;
+		var pM = 10;
+		var pFriction = 0.97;
+		playerOriginalX = canvasWidth/2;
+		playerOriginalY = canvasHeight-150;
+		
+		player = new Star(playerOriginalX,playerOriginalY,pRadius,pM,pFriction);
+		player.player = true;
+		stars.push(player);
+		
+		var outerRing = 8;
+		var ringCount = 3;
+		var ringSpacing = (platformInnerRadius/(ringCount-1));
+		
+		for(var r=0;r<ringCount;r++){
+			var currentRing = 0;
+			var angle = 0;
+			var ringRadius = 0;
+			
+			if(r==ringCount-1){
+				currentRing = 1;
+			}else {
+				currentRing = outerRing-(r*3);
+				angle = 360/currentRing;
+				ringRadius = platformInnerRadius-(ringSpacing*r);
+			};
+			
+			for(var a=0;a<currentRing;a++){
+				var x=0;
+				var y=0;
+				
+				if(r==ringCount-1){
+					x=platformX;
+					y=platformY;
+				}else{
+					x=platformX+(ringRadius*Math.cos((angle*a)*(Math.PI/180)));
+					y=platformY+(ringRadius*Math.sin((angle*a)*(Math.PI/180)));
+				};
+				
+				var radius = 10;
+				var m = 5;
+				var friction = 0.95;
+				stars.push(new Star(x,y,radius,m,friction));
+			};
+		};
+		
+		uiRemaining.html(stars.length-1);
+		uiScore.html("0");
+		uiStats.show();
+		
+		$(window).mousedown(function(e){
+			if(!playerSelected && player.x==playerOriginalX && player.y==playerOriginalY){
+				var canvasOffset = canvas.offset();
+				var canvasX = Math.floor(e.pageX - canvasOffset.left);
+				var canvasY = Math.floor(e.pageY - canvasOffset.top);
+				
+				if(!playGame){
+					playGame = true;
+					animate();
+				};
+				
+				var dX = player.x - canvasX;
+				var dY = player.y - canvasY;
+				var distance = Math.sqrt((dX*dX)+(dY*dY));
+				var padding = 5;
+				
+				if(distance < player.radius+padding){
+					powerX = player.x;
+					powerY = player.y;
+					playerSelected = true;
+				};
+				
+			};
+		});
+		
+		$(window).mousemove(function(e){
+			if(playerSelected){
+				var canvasOffset = canvas.offset();
+				var canvasX = Math.floor(e.pageX -canvasOffset.left);
+				var canvasY = Math.floor(e.pageY - canvasOffset.top);
+				
+				var dX = canvasX-player.x;
+				var dY = canvasY-player.y;
+				var distance=Math.sqrt((dX*dX)+(dY*dY));
+				
+				if(distance*playerVd<playerMaxAbsV){
+					powerX = canvasX;
+					powerY = canvasY;
+				}else{
+					var ratio = playerMaxAbsV/(distance*playerVd);
+					powerX = player.x + (dX*ratio);
+					powerY = player.y + (dY*ratio);
+				};
+			};
+		});
+		
+		$(window).mouseup(function(e){
+			if(playerSelected){
+				var dX = powerX - player.x;
+				var dY = powerY - player.y;
+				player.vX = -(dX*playerVd);
+				player.vY = -(dY*playerVd);
+				uiScore.html(++score);
+			};
+			
+			playerSelected = false;
+			powerX = -1;
+			powerY = -1;
+		});
+		
+		animate();
+	};
+	
+	function init(){
+		uiStats.hide();
+		uiComplete.hide();
+		
+		uiPlay.click(function(e){
+			e.preventDefault();
+			uiInfo.hide();
+			startGame();
+		});
+		
+		uiReset.click(function(e){
+			e.preventDefault();
+			uiComplete.hide();
+			startGame();
+		});
+	};
+	
+	function animate(){
+		context.clearRect(0,0,canvasWidth,canvasHeight);
+		context.fillStyle="rgb(150,150,150)";
+		context.beginPath();
+		context.arc(platformX,platformY,platformOuterRadius,0,Math.PI*2,true);
+		context.closePath();
+		context.fill();
+		
+		if(playerSelected){
+			context.strokeStyle="rgb(220,220,0)";
+			context.lineWidth = 3;
+			context.beginPath();
+			context.moveTo(player.x,player.y);
+			context.lineTo(powerX,powerY);
+			context.closePath();
+			context.stroke();
+		};
+		
+		if(player.x != playerOriginalX && player.y != playerOriginalY){
+			if(player.vX ==0 && player.vY ==0){
+				resetPlayer();
+			}else if(player.x + player.radius<0){
+				resetPlayer();
+			}else if(player.x - player.radius>canvasWidth){
+				resetPlayer();
+			}else if(player.y+player.radius<0){
+				resetPlayer();
+			}else if(player.y-player.radius>canvasHeight){
+				resetPlayer();
+			};
+		};
+		
+		
+		
+		var deadStars = new Array();
+		var starsLength = stars.length;
+		
+		for(var i=0;i<starsLength;i++){
+			var tmpStar = stars[i];
+			
+			for(var j=i+1;j<starsLength;j++){
+				var tmpStarB = stars[j];
+				
+				var dX = tmpStarB.x - tmpStar.x;
+				var dY = tmpStarB.y - tmpStar.y;
+				var distance = Math.sqrt((dX*dX)+(dY*dY));
+				if(distance<tmpStar.radius + tmpStarB.radius){
+					var angle = Math.atan2(dY,dX);
+					var sine = Math.sin(angle);
+					var cosine = Math.cos(angle);
+					
+					var x = 0;
+					var y = 0;
+					
+					var xB = dX * cosine + dY * sine;
+					var yB = dY * cosine - dX * sine;
+					
+					var vX = tmpStar.vX*cosine + tmpStar.vY*sine;
+					var vY = tmpStar.vY*cosine - tmpStar.vX*sine;
+					
+					var vXb = tmpStarB.vX*cosine + tmpStarB.vY*sine;
+					var vYb = tmpStarB.vY*cosine + tmpStarB.vX*sine;
+					
+					var vTotal = vX-vXb;
+					vX=((tmpStar.m - tmpStarB.m)*vX+2*tmpStarB.m*vXb)/(tmpStar.m+tmpStarB.m);
+					vXb = vTotal + vX;
+					
+					xB = x + (tmpStar.radius + tmpStarB.radius);
+					
+					tmpStar.x = tmpStar.x + (x*cosine-y*sine);
+					tmpStar.y = tmpStar.y + (y*cosine+x*sine);
+					
+					tmpStarB.x = tmpStar.x + (xB*cosine-yB*sine);
+					tmpStarB.y = tmpStar.y + (yB*cosine+xB*sine);
+					
+					tmpStar.vX = vX*cosine -vY*sine;
+					tmpStar.vY = vY*cosine + vY*sine;
+					
+					tmpStarB.vX = vXb * cosine - vYb *sine;
+					tmpStarB.vY = vYb * cosine - vXb * sine;
+				}
+				
+			};
+			
+			tmpStar.x += tmpStar.vX;
+			tmpStar.y += tmpStar.vY;
+			
+			if(Math.abs(tmpStar.vX)>0.1){
+				tmpStar.vX *= tmpStar.friction;
+			}else{
+				tmpStar.vX=0;
+			};
+			
+			if(Math.abs(tmpStar.vY)>0.1){
+				tmpStar.vY *= tmpStar.friction;
+			}else{
+				tmpStar.vY = 0;
+			};
+			
+			
+			if(tmpStar.player){
+				context.fillStyle="rgb(200,0,0)";
+			}else{
+				context.fillStyle="rgb(255,255,255)";
+				
+				var dXp = tmpStar.x - platformX;
+				var dYp = tmpStar.y - platformY;
+				var distanceP = Math.sqrt((dXp*dXp) + (dYp*dYp));
+				if(distanceP > platformOuterRadius){
+					if(tmpStar.radius>0){
+						tmpStar.radius -=1;
+					}else{
+						deadStars.push(tmpStar);
+					};
+				};
+			};
+			context.beginPath();
+			context.arc(tmpStar.x,tmpStar.y,tmpStar.radius,0,Math.PI*2,true);
+			context.closePath();
+			context.fill();
+			
+		};
+		
+		var deadStarLength = deadStars.length;
+		if(deadStarLength>0){
+			for(var di=0;di<deadStarLength;di++){
+				var tmpDeadStar = deadStars[di];
+				stars.splice(stars.indexOf(tmpDeadStar),1);
+			};
+			
+			var remaining = stars.length -1;
+			uiRemaining.html(remaining);
+			if(remaining==0){
+				playGame = false;
+				uiStats.hide();
+				uiComplete.show();
+				$(window).unbind("mousedown");
+				$(window).unbind("mouseup");
+				$(window).unbind("mouseover");
+			};
+		};
+		if(playGame){
+			setTimeout(animate,33);
+		};
+	};
+	init();
+});
